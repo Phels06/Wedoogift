f1db2091446315195ef3070511a6abf5cd8cae54
Phels06 <cedric.phelisse@protonmail.com>
2022-07-23 04:32:26 +0200

    feat(structure projet): add projet structure
    Ajoute dependences, properties, gitignore et app principal

---
+++ src/test/java/fr/challenge/wedoogift/WedoogiftApplicationTests.java
@@ -0,0 +1,18 @@
+package fr.challenge.wedoogift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WedoogiftApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void main() {
+        WedoogiftApplication.main(new String[]{});
+    }
+
+}
